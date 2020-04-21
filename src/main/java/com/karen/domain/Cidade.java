76dd434c1cd6cabe6a0e65a3327ca3df3152a7fc
Karen Assunção <karenadf2000@gmail.com>
2020-04-21 20:21:19 -0300

    Atualizacao Json Ignore

--- src/main/java/com/karen/domain/Cidade.java
+++ src/main/java/com/karen/domain/Cidade.java
@@ -9,15 +9,11 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 
 @Entity
 public class Cidade implements Serializable{
 
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 	
 	@Id
@@ -25,7 +21,7 @@ public class Cidade implements Serializable{
 	private Integer id;
 	private String nome;
 	
-	@JsonManagedReference
+	
 	@ManyToOne
 	@JoinColumn(name="estado_id")
 	private Estado estado;
